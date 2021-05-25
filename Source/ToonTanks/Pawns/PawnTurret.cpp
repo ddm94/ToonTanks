616377f38673a35db894db72a5f7c56349a26673
Daniele Di Modica <d.dimo94@hotmail.com>
2021-05-25 18:52:55 +0100

    Added PawnTurret class and implemented basic check for future fire functionality.

---
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h" // To implement timer functionalities
+#include "PawnTank.h"
+
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+    // GetWorldTimerManager() Same thing as above
+
+    // Cast - Cross class communication - allows us to convert a provided type to another using the built-in reflection system in UE
+    // Here we are passing a generic Pawn class, which is the base class of all controllable classes and then we'll be converting it
+    // to check whether it is not only APawn but it is a type of APawnTank class
+    // This allows us to access any exposed Tank-specific functionalities since we'll have a reference to not just APawn but the specific Tank version.
+    PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void APawnTurret::CheckFireCondition()
+{
+    // If Player == null || is Dead then BAIL!!
+    if (!PlayerPawn)
+    {
+        return;
+    }
+    
+
+    // If Player IS in range then FIRE!!
+    if (ReturnDistanceToPlayer() <= FireRange)
+    {
+        // Fire
+        // UE_LOG(LogTemp, Warning, TEXT("Fire Condition Success"));
+    }
+    
+    // UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked"));
+}
+
+float APawnTurret::ReturnDistanceToPlayer()
+{
+        if (!PlayerPawn)
+    {
+        return 0.0f;
+    }
+
+    // Remember to include PawnTank.h if you want to use its functionalities
+    return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
+
+}
---
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTurret.generated.h"
+
+/**
+ * Access Modifier
+ * 
+ * Private: most secure - nothing outside of the class can access a property marked as private
+ * Public: least secure - allows any class with a valid reference to the second class then access these properties which were declared inside
+ * Protected: unique - properties that need to be hidden from outside classes but need to be available in derived child classes
+ */
+
+class APawnTank;
+
+UCLASS()
+class TOONTANKS_API APawnTurret : public APawnBase
+{
+	GENERATED_BODY()
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	// Allows to bind and unbind our timer which will be either looping or called at certain intervals
+	FTimerHandle FireRateTimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRate = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRange = 500.f;
+
+	APawnTank* PlayerPawn;
+
+	void CheckFireCondition();
+
+	float ReturnDistanceToPlayer();
+
+};
